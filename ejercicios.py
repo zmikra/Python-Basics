849e92d439caac90ad6974229b91af0e6ab9317d
Micaela Rodríguez <mrodriguez111194@gmail.com>
2024-12-27 16:46:58 +0100

    first init

---
+++ ejercicios.py
@@ -0,0 +1,9 @@
+#Suma de dos números pedidos al usuario
+
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+result = num1 + num2
+
+print("El restulado de la suma es: ", result)
+
