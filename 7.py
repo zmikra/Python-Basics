1dac068932320ecef77efdbdf4bf8d7f4943a55d
Micaela Rodríguez <mrodriguez111194@gmail.com>
2025-01-13 16:54:30 +0100

    7
    lettercount excercise

---
+++ 7.py
@@ -0,0 +1,9 @@
+#8. Contar letras en una palabra
+#Ejercicio: Pide al usuario una palabra y cuenta cuántas letras tiene.
+
+#Explicación: Práctica con cadenas y la función len().
+
+word = input("Ingrese una palabra: ")
+lettercount = len(word)
+
+print("La palabra ingresada tiene ", lettercount, "letras.")
